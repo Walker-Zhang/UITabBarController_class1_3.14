8866b7922f0512640cd201d2f543c91f5f3a8ced
xiang_jj <xiang_jj@xiang-jjtekiMacBook-Air.local>
2014-03-14 09:12:36 +0800

    Initial Commit

---
+++ UITabBarController_class1_3.14/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UITabBarController_class1_3.14
+//
+//  Created by xiang_jj on 14-3-14.
+//  Copyright (c) 2014年 xiang_jj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITabBarController_class1_3.14/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITabBarController_class1_3.14
+//
+//  Created by xiang_jj on 14-3-14.
+//  Copyright (c) 2014年 xiang_jj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
